badf1fad7daaeafa6e5cce1e4a06a33b3717aedb
Bjorn Harpe <bjorn.harpe@snapsecurity.ca>
2015-10-16 14:26:53 -0600

    making tests pass

--- bucky/statsd.py
+++ bucky/statsd.py
@@ -21,14 +21,23 @@ import time
 import json
 import logging
 import threading
+import multiprocessing
 
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+try:
+    from setproctitle import setproctitle
+except ImportError:
+    def setproctitle(title):
+        pass
 import six
 
 import bucky.udpserver as udpserver
 from bucky.errors import ConfigError, ProtocolError
-import bucky.cfg as cfg
 
-
+#log = multiprocessing.log_to_stderr(logging.DEBUG)
 log = logging.getLogger(__name__)
 
 try:
@@ -72,23 +81,24 @@ def make_name(parts):
 
 
 class StatsDHandler(threading.Thread):
-    def __init__(self, queue, cfg):
+    def __init__(self, outbox, config):
         super(StatsDHandler, self).__init__()
         self.daemon = True
-        self.queue = queue
-        self.cfg = cfg
+        self.outbox = outbox
+        self.inbox = queue.Queue()
+        self.cfg = config
         self.lock = threading.Lock()
         self.timers = {}
         self.gauges = {}
         self.counters = {}
         self.sets = {}
-        self.flush_time = cfg.statsd_flush_time
-        self.legacy_namespace = cfg.statsd_legacy_namespace
-        self.global_prefix = cfg.statsd_global_prefix
-        self.prefix_counter = cfg.statsd_prefix_counter
-        self.prefix_timer = cfg.statsd_prefix_timer
-        self.prefix_gauge = cfg.statsd_prefix_gauge
-        self.prefix_set = cfg.statsd_prefix_set
+        self.flush_time = config.statsd_flush_time
+        self.legacy_namespace = config.statsd_legacy_namespace
+        self.global_prefix = config.statsd_global_prefix
+        self.prefix_counter = config.statsd_prefix_counter
+        self.prefix_timer = config.statsd_prefix_timer
+        self.prefix_gauge = config.statsd_prefix_gauge
+        self.prefix_set = config.statsd_prefix_set
         self.key_res = (
             (re.compile("\s+"), "_"),
             (re.compile("\/"), "-"),
@@ -109,15 +119,16 @@ class StatsDHandler(threading.Thread):
             self.name_gauge = make_name([self.global_prefix, self.prefix_gauge])
             self.name_set = make_name([self.global_prefix, self.prefix_set])
 
-        self.statsd_persistent_gauges = cfg.statsd_persistent_gauges
+        self.statsd_persistent_gauges = config.statsd_persistent_gauges
         self.gauges_filename = os.path.join(self.cfg.directory, self.cfg.statsd_gauges_savefile)
 
         self.keys_seen = set()
-        self.delete_idlestats = cfg.statsd_delete_idlestats
-        self.delete_counters = self.delete_idlestats and cfg.statsd_delete_counters
-        self.delete_timers = self.delete_idlestats and cfg.statsd_delete_timers
-        self.delete_sets = self.delete_idlestats and cfg.statsd_delete_sets
-        self.onlychanged_gauges = self.delete_idlestats and cfg.statsd_onlychanged_gauges
+        self.delete_idle_stats = config.statsd_delete_idlestats
+        self.delete_counters = self.delete_idle_stats and config.statsd_delete_counters
+        self.delete_timers = self.delete_idle_stats and config.statsd_delete_timers
+        self.delete_sets = self.delete_idle_stats and config.statsd_delete_sets
+        self.only_changed_gauges = self.delete_idle_stats and config.statsd_onlychanged_gauges
+        self.next_update = time.time() + self.flush_time
 
     def load_gauges(self):
         if not self.statsd_persistent_gauges:
@@ -141,89 +152,121 @@ class StatsDHandler(threading.Thread):
         except IOError:
             log.exception("StatsD: IOError")
 
+    def _flush(self):
+        name_global_num_stats = self.name_global + "numStats"
+        stat_time = int(time.time())
+        with self.lock:
+            if self.delete_timers:
+                rem_keys = set(self.timers.keys()) - self.keys_seen
+                for k in rem_keys:
+                    del self.timers[k]
+            if self.delete_counters:
+                rem_keys = set(self.counters.keys()) - self.keys_seen
+                for k in rem_keys:
+                    del self.counters[k]
+            if self.delete_sets:
+                rem_keys = set(self.sets.keys()) - self.keys_seen
+                for k in rem_keys:
+                    del self.sets[k]
+            num_stats = self.enqueue_timers(stat_time)
+            num_stats += self.enqueue_counters(stat_time)
+            num_stats += self.enqueue_gauges(stat_time)
+            num_stats += self.enqueue_sets(stat_time)
+            self.enqueue(name_global_num_stats, num_stats, stat_time)
+            self.keys_seen = set()
+        return num_stats
+
+    @staticmethod
+    def debug_msg(message, loop_count, *args):
+        msg = '[%f] %s - %d ' + message
+        log.debug(msg, time.time(), threading.current_thread().ident, loop_count, *args)
+
     def run(self):
-        name_global_numstats = self.name_global + "numStats"
+        setproctitle("bucky: %s" % self.__class__.__name__)
+        loop_count = 0
         while True:
-            time.sleep(self.flush_time)
-            stime = int(time.time())
-            with self.lock:
-                if self.delete_timers:
-                    rem_keys = set(self.timers.keys()) - self.keys_seen
-                    for k in rem_keys:
-                        del self.timers[k]
-                if self.delete_counters:
-                    rem_keys = set(self.counters.keys()) - self.keys_seen
-                    for k in rem_keys:
-                        del self.counters[k]
-                if self.delete_sets:
-                    rem_keys = set(self.sets.keys()) - self.keys_seen
-                    for k in rem_keys:
-                        del self.sets[k]
-                num_stats = self.enqueue_timers(stime)
-                num_stats += self.enqueue_counters(stime)
-                num_stats += self.enqueue_gauges(stime)
-                num_stats += self.enqueue_sets(stime)
-                self.enqueue(name_global_numstats, num_stats, stime)
-                self.keys_seen = set()
+            loop_count += 1
+            to_sleep = self.next_update - time.time()
+            self.debug_msg('Loop Start to_sleep : %f\tnext_update: %f\tflush_time: %f', loop_count, to_sleep,
+                           self.next_update, self.flush_time)
+            if to_sleep <= 0:
+                self.debug_msg('Flushing stats', loop_count)
+                self._flush()
+                self.next_update = time.time() + self.flush_time
+                to_sleep = self.flush_time
+                self.debug_msg('Post Flush to_sleep : %f\tnext_update: %f\tflush_time: %f', loop_count, to_sleep,
+                               self.next_update, self.flush_time)
+            try:
+                self.debug_msg('Pre Queue to_sleep : %f\tnext_update: %f\tflush_time: %f', loop_count, to_sleep,
+                               self.next_update, self.flush_time)
+                metric = self.inbox.get(True, to_sleep)
+                if metric is None:
+                    log.info("Handler received None, %s exiting", self)
+                    break
+                self.debug_msg('Handling metric %r ', loop_count, metric)
+                self.handle(metric)
+            except queue.Empty:
+                self.debug_msg('Empty Queue', loop_count)
+                continue
 
-    def enqueue(self, name, stat, stime):
+    def enqueue(self, name, stat, stat_time):
         # No hostnames on statsd
-        self.queue.put((None, name, stat, stime))
+        self.outbox.put((None, name, stat, stat_time))
 
-    def enqueue_timers(self, stime):
+    def enqueue_timers(self, stat_time):
         ret = 0
         iteritems = self.timers.items() if six.PY3 else self.timers.iteritems()
         for k, v in iteritems:
             # Skip timers that haven't collected any values
             if not v:
-                self.enqueue("%s%s.count" % (self.name_timer, k), 0, stime)
-                self.enqueue("%s%s.count_ps" % (self.name_timer, k), 0.0, stime)
+                self.enqueue("%s%s.count" % (self.name_timer, k), 0, stat_time)
+                self.enqueue("%s%s.count_ps" % (self.name_timer, k), 0.0, stat_time)
             else:
                 v.sort()
                 pct_thresh = 90
                 count = len(v)
-                vmin, vmax = v[0], v[-1]
-                mean, vthresh = vmin, vmax
+                value_min, value_max = v[0], v[-1]
+                mean, value_thresh = value_min, value_max
 
                 if count > 1:
                     thresh_idx = int(math.floor(pct_thresh / 100.0 * count))
                     v = v[:thresh_idx]
-                    vthresh = v[-1]
-                    vsum = sum(v)
-                    mean = vsum / float(len(v))
+                    value_thresh = v[-1]
+                    value_sum = sum(v)
+                    mean = value_sum / float(len(v))
 
-                self.enqueue("%s%s.mean" % (self.name_timer, k), mean, stime)
-                self.enqueue("%s%s.upper" % (self.name_timer, k), vmax, stime)
+                self.enqueue("%s%s.mean" % (self.name_timer, k), mean, stat_time)
+                self.enqueue("%s%s.upper" % (self.name_timer, k), value_max, stat_time)
                 t = int(pct_thresh)
-                self.enqueue("%s%s.upper_%s" % (self.name_timer, k, t), vthresh, stime)
-                self.enqueue("%s%s.lower" % (self.name_timer, k), vmin, stime)
-                self.enqueue("%s%s.count" % (self.name_timer, k), count, stime)
-                self.enqueue("%s%s.count_ps" % (self.name_timer, k), float(count) / self.flush_time, stime)
+                self.enqueue("%s%s.upper_%s" % (self.name_timer, k, t), value_thresh, stat_time)
+                self.enqueue("%s%s.lower" % (self.name_timer, k), value_min, stat_time)
+                self.enqueue("%s%s.count" % (self.name_timer, k), count, stat_time)
+                self.enqueue("%s%s.count_ps" % (self.name_timer, k), float(count) / self.flush_time, stat_time)
             self.timers[k] = []
             ret += 1
 
         return ret
 
-    def enqueue_sets(self, stime):
+    def enqueue_sets(self, stat_time):
         ret = 0
         iteritems = self.sets.items() if six.PY3 else self.sets.iteritems()
         for k, v in iteritems:
-            self.enqueue("%s%s.count" % (self.name_set, k), len(v), stime)
+            self.enqueue("%s%s.count" % (self.name_set, k), len(v), stat_time)
             ret += 1
             self.sets[k] = set()
         return ret
 
-    def enqueue_gauges(self, stime):
+    def enqueue_gauges(self, stat_time):
         ret = 0
         iteritems = self.gauges.items() if six.PY3 else self.gauges.iteritems()
         for k, v in iteritems:
             # only send a value if there was an update if `delete_idlestats` is `True`
-            if not self.onlychanged_gauges or k in self.keys_seen:
-                self.enqueue("%s%s" % (self.name_gauge, k), v, stime)
+            if not self.only_changed_gauges or k in self.keys_seen:
+                self.enqueue("%s%s" % (self.name_gauge, k), v, stat_time)
                 ret += 1
         return ret
 
-    def enqueue_counters(self, stime):
+    def enqueue_counters(self, stat_time):
         ret = 0
         iteritems = self.counters.items() if six.PY3 else self.counters.iteritems()
         for k, v in iteritems:
@@ -233,8 +276,8 @@ class StatsDHandler(threading.Thread):
             else:
                 stat_rate = "%s%s.rate" % (self.name_counter, k)
                 stat_count = "%s%s.count" % (self.name_counter, k)
-            self.enqueue(stat_rate, v / self.flush_time, stime)
-            self.enqueue(stat_count, v, stime)
+            self.enqueue(stat_rate, v / self.flush_time, stat_time)
+            self.enqueue(stat_count, v, stat_time)
             self.counters[k] = 0
             ret += 1
         return ret
@@ -251,11 +294,14 @@ class StatsDHandler(threading.Thread):
         else:
             log.error('Unhandled metric type "%s"', metric.type)
 
+    def put(self, metric):
+        log.debug('Handler Received %r', metric)
+        self.inbox.put(metric)
+
     def handle_timer(self, metric):
         try:
-            val = float(metric.value or 0)
             with self.lock:
-                self.timers.setdefault(metric.name, []).append(metric.value)
+                self.timers.setdefault(metric.name, []).append(float(metric.value or 0))
         except:
             self.bad_metric(metric)
 
@@ -272,11 +318,11 @@ class StatsDHandler(threading.Thread):
 
     def handle_set(self, metric):
         try:
-            valstr = str(metric.value or "0")
+            value_string = str(metric.value or "0")
             with self.lock:
                 if metric.name not in self.sets:
                     self.sets[metric.name] = set()
-                self.sets[metric.name].add(valstr)
+                self.sets[metric.name].add(value_string)
         except:
             self.bad_metric(metric)
 
@@ -330,7 +376,9 @@ class StatsDParser(object):
     def __init__(self):
         pass
 
-    def parse(self, data):
+    @staticmethod
+    def parse(data):
+        log.debug('Parsing : %r', data)
         if six.PY3:
             data = data.decode()
         for line in data.splitlines():
@@ -338,15 +386,17 @@ class StatsDParser(object):
             for sample in data.split(':'):
                 args = sample.split('|')
                 metric = StatsDMetric(name, *args)
+                log.debug('Received : %r', metric)
                 yield metric
 
 
 class StatsDServer(udpserver.UDPServer):
-    def __init__(self, queue, cfg):
-        super(StatsDServer, self).__init__(cfg.statsd_ip, cfg.statsd_port)
+    def __init__(self, inbox, config):
+        super(StatsDServer, self).__init__(config.statsd_ip, config.statsd_port)
         self.parser = StatsDParser()
-        self.handlers = self._init_handlers(queue, cfg)
-        log.debug('Handlers: %r', self.handlers)
+        self.config = config
+        self.inbox = inbox
+        self.handlers = self._init_handlers(self.inbox, self.config)
 
     def pre_shutdown(self):
         for _, handler in self.handlers:
@@ -354,23 +404,29 @@ class StatsDServer(udpserver.UDPServer):
 
     def run(self):
         log.debug('Starting Run Method')
+        for _, handler in self.handlers:
+            handler.start()
         super(StatsDServer, self).run()
 
     def handle(self, data, addr):
+        log.debug('Received %r from %r', data, addr)
         try:
             for mc in self.parser.parse(data):
                 handler = self._get_handler(mc.name)
-                handler.handle(mc)
+                log.debug('Handling %r with handler %r', mc, handler)
+                handler.put(mc)
         except ProtocolError:
             log.exception("Error from: %s:%s" % addr)
+        return True
 
-    def _init_handlers(self, queue, cfg):
+    @staticmethod
+    def _init_handlers(inbox, config):
         ret = []
-        handlers = cfg.metricsd_handlers
+        handlers = config.metricsd_handlers
         if not len(handlers):
-            ret = [(None, StatsDHandler(queue, cfg))]
-            ret[0][1].start()
+            ret = [(None, StatsDHandler(inbox, config))]
             return ret
+        handlers.sort(key=lambda p: p[2])
         for item in handlers:
             if len(item) == 2:
                 pattern, interval, priority = item[0], item[1], 100
@@ -385,11 +441,8 @@ class StatsDServer(udpserver.UDPServer):
             if interval < 0:
                 raise ConfigError("Invalid interval: %s" % interval)
             ret.append((pattern, interval, priority))
-        handlers.sort(key=lambda p: p[2])
-        ret = [(p, StatsDHandler(queue, i)) for (p, i, _) in ret]
-        ret.append((None, StatsDHandler(queue, cfg)))
-        for _, h in ret:
-            h.start()
+        ret = [(p, StatsDHandler(inbox, i)) for (p, i, _) in ret]
+        ret.append((None, StatsDHandler(inbox, config)))
         return ret
 
     def _get_handler(self, name):
@@ -400,7 +453,4 @@ class StatsDServer(udpserver.UDPServer):
                 return h
 
     def close(self):
-        for _, handler in self.handlers:
-            handler.close()
-            handler.join(cfg.process_join_timeout)
         super(StatsDServer, self).close()
